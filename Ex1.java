079983cb94bb31efe396d89a7c4634bb380a8343
희진 <jhj090120@gmail.com>
2025-01-21 14:47:52 +0900

    자바 수행평가 1번제

--- Ex1.java
+++ Ex1.java
@@ -1,26 +1,50 @@
-package InT;
+package hi;
+//1116 정희진
+import java.util.Random;
 
-class sStaticSample {
-	public int n;
-	public void g() {
-		m = 20;
-	}
-	public void h() {
-		m = 30;
-	}
-	public static int m;
-	public static void f() {
-		m = 5;
-	}
-}
 public class Ex1 {
-	public static void main(String args[]) {
-		sStaticSample.m = 10;
-		sStaticSample s1;
-		
-		s1 = new sStaticSample();
-		System.out.println(s1.m);
-		s1.f();
-		sStaticSample.f();
-	}
+    class Point {
+        private int x;
+        private int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Circle {
+        private Point center;
+        private int radius;
+
+        public Circle(Point center, int radius) {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public double Area() {
+            return Math.PI * radius * radius;
+        }
+
+        public void printCircle() {
+            System.out.printf("점의 좌표<%d, %d>인 반지름 %d인 원의 넓이 : %.3f\n",
+                    center.x, center.y, radius, Area());
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex1 ex = new Ex1();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 4; i++) {
+            int x = random.nextInt(101);
+            int y = random.nextInt(101);
+            int radius = random.nextInt(10) + 1;
+
+            Point p = ex.new Point(x, y);
+            Circle cir = ex.new Circle(p, radius);
+            cir.printCircle();
+        }
+    }
 }
